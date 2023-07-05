c48d685fef6cc994e86298042b7710cc07a26722
Nishant Rai <rai7nishant@gmail.com>
2023-07-05 11:01:01 +0530

    Filtering using normal JS variable

--- FoodGallore/src/components/Body.js
+++ FoodGallore/src/components/Body.js
@@ -5,7 +5,12 @@ import RestaurantCard from "./RestaurantCard";
 const Body = () => {
     return (
         <div className="body">
-            <div className="Filter-btn" onClick={() => { console.log("button clicked") }}>Top Rated Restaurants</div>
+            <div className="Filter-btn" onClick={() => {
+                console.log(
+                    resList.filter((restraunt) => {
+                        return restraunt.data.avgRating > 4
+                    }))
+            }}>Top Rated Restaurants</div>
             <div className="restaurant-container">
 
 
