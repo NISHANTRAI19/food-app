60a6546f86385ed64f8a0a907bb4ee49f2aaadce
Nishant Rai <rai7nishant@gmail.com>
2023-07-14 15:15:44 +0530

    small change in css and classes

--- FoodGallore/src/components/About.js
+++ FoodGallore/src/components/About.js
@@ -4,11 +4,11 @@ import UserClass from "./UserClass";
 class About extends React.Component {
     constructor(props) {
         super(props);
-        console.log("parent constructor");
+
     }
 
     render() {
-        console.log("Parent rendered");
+
         return (<UserClass />);
     }
 }
--- FoodGallore/src/components/UserClass.js
+++ FoodGallore/src/components/UserClass.js
@@ -32,7 +32,7 @@ class UserClass extends React.Component {
 
         return (
             <div className="user-card">
-                <img src={avatar_url}></img>
+                <img className="dev-avatar" src={avatar_url}></img>
                 <h2>Name:{name}</h2>
                 <h3>Location:{location}</h3>
                 <h4 onClick={() => {
