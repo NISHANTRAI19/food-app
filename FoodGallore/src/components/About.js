45bb80b136004d266aab47fdec62a57ce610c9de
Nishant Rai <rai7nishant@gmail.com>
2023-07-14 12:59:50 +0530

    Fetch dev data
    fetched data using swiggy API

--- FoodGallore/src/components/About.js
+++ FoodGallore/src/components/About.js
@@ -2,8 +2,14 @@ import React from "react";
 import UserClass from "./UserClass";
 
 class About extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("parent constructor");
+    }
+
     render() {
-        return (<UserClass name={"Nishant"} location={"Jhakri"} />);
+        console.log("Parent rendered");
+        return (<UserClass />);
     }
 }
 export default About;
--- FoodGallore/src/components/UserClass.js
+++ FoodGallore/src/components/UserClass.js
@@ -1,16 +1,38 @@
 import React from "react";
+
 class UserClass extends React.Component {
 
     constructor(props) {
-        super(props);
+
+        super(props)
+
         this.state = {
-            count: 0,
+
+            user: {
+                name: "Dummy",
+                photo: "abc",
+                location: "Bangalore"
+
+            }
         }
     }
+    async componentDidMount() {
+        const api = await fetch("https://api.github.com/users/nishantrai19");
+        const json = await api.json();
+        this.setState({
+            user: json,
+        })
+
+
+    }
+
     render() {
-        const { name, location } = this.props
+
+        const { name, location, avatar_url } = this.state.user;
+
         return (
             <div className="user-card">
+                <img src={avatar_url}></img>
                 <h2>Name:{name}</h2>
                 <h3>Location:{location}</h3>
                 <h4 onClick={() => {
@@ -21,7 +43,9 @@ class UserClass extends React.Component {
 
 
 
-            </div>
+
+
+            </div >
         )
     }
 }
